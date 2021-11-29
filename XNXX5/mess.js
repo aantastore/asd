f5944f796c425d193a69b406b9fa8806b4d7c05e
surantasembiringkembarens <91650298+aantastore@users.noreply.github.com>
2021-11-29 14:40:47 +0700

    Add files via upload

---
+++ XNXX5/mess.js
@@ -0,0 +1,19 @@
+//UBAH SESUAI KALIAN MAU
+
+module.exports.mess = {
+	        wait: 'PROSES...',
+			success: 'NI HASILNYA KAK',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER TOLOL',
+				Iv: 'HEII ASTGA UDAH LOH'
+			},
+			only: {
+				group: 'INI DI GRUB KAH? KALAU BUKAN DI GRUB GBSA BANNG',
+				admin: 'KAMU ADMIN?',
+				premium: 'lwcks Bli dlu',
+				owner: 'LU OWNER BRO',
+				Badmin: 'BOT HARUS JADI ADMIN BRO inget bukan ngemis',
+			}
+		}
